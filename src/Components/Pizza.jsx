3c9f0b9a7f7caec0b6eb9ca9cba9ecb1b245741f
Raja0514 <rajamr1987@gmail.com>
2022-07-12 18:12:01 +0400

    add to cart

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -1,72 +1,90 @@
-import React from 'react'
-import {Card,Row,Col} from 'react-bootstrap'
-import { useState } from 'react'
+import React from "react";
+import { Card, Row, Col, Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
-const Pizza = ({pizza}) => {
-const[varient,setVarient]=useState('small')
-const[quantity,setQuantity]=useState(1)
+const Pizza = ({ pizza }) => {
+  const [varient, setVarient] = useState("small");
+  const [quantity, setQuantity] = useState(1);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-     <Card style={{ width: '18rem' ,marginTop:'30px' }}>
-      <Card.Img variant="top" src={pizza.image} />
-      <Card.Body>
-        <Card.Title>{pizza.name}</Card.Title>
-        <Card.Text>
-          <Row>
-            <Col md={6}>
+      <Card style={{ width: "18rem", marginTop: "30px" }}>
+        <Card.Img
+          variant="top"
+          onClick={handleShow}
+          src={pizza.image}
+          style={{ height: "220px", cursor: "pointer" }}
+        />
+        <Card.Body>
+          <Card.Title>{pizza.name}</Card.Title>
+          <hr></hr>
+          <Card.Text>
+            <Row>
+              <Col md={6}>
                 <h6>Varients</h6>
                 <select
-
-value={varient}
-
-                onChange={e=>setVarient(e.target.value)}
-
+                  value={varient}
+                  onChange={(e) => setVarient(e.target.value)}
                 >
-                    {pizza.varients.map((varient)=>{
-                        return(
-                            <>
-                            <option 
-                             
-                             
-                            >{varient}</option>
-                            </>
-                        )
-
-                    })}
+                  {pizza.varients.map((varient) => {
+                    return (
+                      <>
+                        <option>{varient}</option>
+                      </>
+                    );
+                  })}
                 </select>
-            </Col>
-            <Col md={6}>
+              </Col>
+              <Col md={6}>
                 <h6>Quantitity</h6>
-                <select value={quantity}
-                onChange={e=>setQuantity(e.target.value)}
+                <select
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
                 >
-                {[...Array(10).keys()].map((v,i)=>{
-                    return(
-                        <>
-                        <option value={i+1}
-                        
-                        >
-                        {i+1}
-                        </option>
-                        </>
-                    )
-                })}
-
+                  {[...Array(10).keys()].map((v, i) => {
+                    return (
+                      <>
+                        <option value={i + 1}>{i + 1}</option>
+                      </>
+                    );
+                  })}
                 </select>
-                
+              </Col>
+            </Row>
+          </Card.Text>
+          <Row>
+            <Col md={6}>price:{pizza.prices[0][varient] * quantity}-/RS</Col>
+            <Col>
+              <Button className="bg-dark text-white">Add to Cart</Button>
             </Col>
           </Row>
-        </Card.Text>
-        <Row>
-            <Col md={6}>price:{pizza.prices[0][varient]*quantity}</Col>
-            <Col>
-            </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+      {/* <p>Model</p> */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <Card.Img
+              variant="top"
+              src={pizza.image}
+              style={{ height: "220px" }}
+            />
+          </div>
+          <div>
+            <h5>Description:</h5>
+            <h6>{pizza.description}</h6>
+          </div>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </>
-  )
-}
-
-export default Pizza
+  );
+};
+export default Pizza;
